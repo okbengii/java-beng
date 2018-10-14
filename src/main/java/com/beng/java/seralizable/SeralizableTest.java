1e26d7ce3787f85bb50f62d04681a3eac0ea11d2
baiyaguang <baiyaguang@finupgrooup.com>
2018-10-14 21:26:18 +0800

    引用对象序列化

--- src/main/java/com/beng/java/seralizable/SeralizableTest.java
+++ src/main/java/com/beng/java/seralizable/SeralizableTest.java
@@ -27,7 +27,7 @@ public class SeralizableTest {
         }
 
         // 读取
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("object.txt"))) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("src/main/resources/object.txt"))) {
             Person p1 = (Person) ois.readObject();
             System.out.println(p1.getName());
         } catch (FileNotFoundException e) {
@@ -35,6 +35,24 @@ public class SeralizableTest {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        /*
+         * 引用对象序列化 如果 Teacher 对象没有实现 seralizable 接口
+         * 报错：java.io.NotSerializableException
+         */
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("src/main/resources/object1.txt"))) {
+            Person p = new Person("Jack", "18");
+            Teacher t1 = new Teacher("Tom", p);
+            Teacher t2 = new Teacher("Janny", p);
+            os.writeObject(p);
+            os.writeObject(t1);
+            os.writeObject(t2);
+            os.writeObject(p);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
---
+++ src/main/java/com/beng/java/seralizable/Teacher.java
@@ -0,0 +1,32 @@
+package com.beng.java.seralizable;
+
+import java.io.Serializable;
+
+public class Teacher implements Serializable {
+
+    String name;
+
+    Person p;
+
+    public Teacher(String name, Person p) {
+        this.name = name;
+        this.p = p;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getP() {
+        return p;
+    }
+
+    public void setP(Person p) {
+        this.p = p;
+    }
+
+}
