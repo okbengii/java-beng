13f573b6c05e99e26833045c3cd8187110219f05
baiyaguang <301070179@qq.com>
2019-06-26 22:59:47 +0800

    java 线程安全

---
+++ src/main/java/com/beng/thread/safe/NotifyTest.java
@@ -0,0 +1,54 @@
+package com.beng.thread.safe;
+
+/**
+ * 使用 notifyAll 不要使用 notify
+ * 
+ * @author apple
+ */
+public class NotifyTest implements Runnable {
+
+    private static int time = 0;
+
+    private static final Object lock = new Object();
+
+    private final int step;
+
+    private NotifyTest(int step) {
+        this.step = step;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            synchronized (lock) {
+                while (time != step) {
+                    System.out.println(Thread.currentThread().getName() + " " + time);
+                    lock.wait();
+                }
+                System.out.println(Thread.currentThread().getName() + " " + time);
+
+                time++;
+                System.out.println(Thread.currentThread().getName() + " " + time);
+                lock.notify();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupted();
+        }
+    }
+
+    /**
+     * 容易造成死锁
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            new Thread(new NotifyTest(i)).start();
+        }
+
+    }
+
+}
