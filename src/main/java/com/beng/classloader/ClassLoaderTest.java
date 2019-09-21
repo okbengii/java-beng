115d1dde645e5255313e4f07fa4e3aa6878bfe92
baiyaguang <301070179@qq.com>
2019-09-21 23:47:11 +0800

    测试类加载器

---
+++ src/main/java/com/beng/classloader/ClassLoaderTest.java
@@ -0,0 +1,33 @@
+package com.beng.classloader;
+
+import java.io.InputStream;
+
+public class ClassLoaderTest {
+
+    public static void main(String[] args)
+            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+
+        ClassLoader classLoader = new ClassLoader() {
+
+            @Override
+            public Class<?> loadClass(String name) throws ClassNotFoundException {
+                try {
+                    String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+                    InputStream is = getClass().getResourceAsStream(fileName);
+                    if (is == null) {
+                        return super.loadClass(name);
+                    }
+                    byte[] b = new byte[is.available()];
+                    is.read(b);
+                    return defineClass(name, b, 0, b.length);
+                } catch (Exception e) {
+                    throw new ClassNotFoundException(name);
+                }
+            }
+        };
+
+        Object obj = classLoader.loadClass("com.beng.classloader.SubClass").newInstance();
+        System.out.println(obj.getClass());
+        System.out.println(obj instanceof SubClass);
+    }
+}
